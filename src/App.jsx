8b98f9b823f760f8b6eb92b71f950ce444690445
isabella <isabella24ti@mahasiswa.pcr.ac.id>
2026-05-04 17:37:09 +0700

    Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,48 @@
-import React, { useEffect } from 'react';
-import {
-  Routes,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
-import './css/style.css';
+import "./css/style.css";
 
-import './charts/ChartjsConfig';
+import "./charts/ChartjsConfig";
 
 // Import pages
-import Dashboard from './pages/Dashboard';
+import Dashboard from "./pages/Dashboard";
 
+import KelolaFasilitas from "./pages/pegawai/KelolaFasilitas";
+import KelolaPegawai from "./pages/pegawai/KelolaPegawai";
+
+import KelolaBarang from "./pages/pegawai/KelolaBarang";
+import KelolaRuangan from "./pages/pegawai/KelolaRuangan";
+import KelolaLaboratorium from "./pages/pegawai/KelolaLaboratorium";
+
+import PeminjamanBarang from "./pages/pegawai/PeminjamanBarang";
+import PeminjamanRuangan from "./pages/pegawai/PeminjamanRuangan";
+import PeminjamanLaboratorium from "./pages/pegawai/PeminjamanLaboratorium";
 function App() {
-
   const location = useLocation();
 
   useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
   }, [location.pathname]); // triggered on route change
 
   return (
     <>
       <Routes>
-        <Route exact path="/" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
+
+        <Route path="/pegawai" element={<KelolaPegawai />} />
+
+        {/* Fasilitas */}
+        <Route path="/fasilitas/barang" element={<KelolaFasilitas />} />
+        <Route path="/fasilitas/ruangan" element={<KelolaRuangan />} />
+        <Route path="/fasilitas/laboratorium" element={<KelolaLaboratorium />} />
+
+        {/* Peminjaman */}
+        <Route path="/peminjaman/barang" element={<PeminjamanBarang />} />
+        <Route path="/peminjaman/ruangan" element={<PeminjamanRuangan />} />
+        <Route path="/peminjaman/laboratorium" element={<PeminjamanLaboratorium />} />
       </Routes>
     </>
   );
